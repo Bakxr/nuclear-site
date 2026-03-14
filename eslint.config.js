c07a58d28b6cc4c15ef9e4902620010c93979f96
Your Name <your-email@example.com>
2026-03-14 19:30:55 -0400

    Ignore Vercel build output in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,11 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    '.vercel',
+    '.vercel/**',
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
